a7152e6637043913101eab6639094dcacff57bd6
NoSleepFox <SimenCodemaster@proton.me>
2026-03-12 22:21:40 +0100

    a) nesten fullført, tiltabell må fikses

---
+++ src/main/java/com/oppgaver/TabellMengde.java
@@ -0,0 +1,148 @@
+package com.oppgaver;
+
+import java.util.Arrays;
+
+public class TabellMengde<T> implements MengdeADT<T>{
+    
+    private static final int DEFAULT_KAPASITET = 10;
+
+	private T[] tabell;
+	private int antall;
+
+	/************************************************************/
+
+	public TabellMengde() {
+		this(DEFAULT_KAPASITET);
+	}
+
+	@SuppressWarnings("unchecked")
+	public TabellMengde(int kapasitet) {
+		tabell = (T[]) new Object[kapasitet];
+		antall = 0;
+	}
+
+	/************************************************************/
+
+	@Override
+	public boolean erTom() {
+		return antall == 0;
+	}
+
+	@Override
+	public boolean inneholder(T anEntry) {
+        for (int i = 0; i < antall; i++) {
+            if (anEntry.equals(tabell[i])) {
+                return true;
+            }
+        }
+        return false;
+	}
+
+    @Override
+    public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
+        for (int i = 0; i < antall; i++) {
+            if(!annenMengde.inneholder(tabell[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+        for (int i = 0; i < antall; i++) {
+            if(annenMengde.inneholder(tabell[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public boolean erLik(MengdeADT<T> annenMengde) {
+        return erDelmengdeAv(annenMengde) && annenMengde.erDelmengdeAv(this);
+    }
+
+    @Override
+    public MengdeADT<T> snitt(MengdeADT<T> annenMengde){
+        TabellMengde<T> returnMengde = new TabellMengde<>();
+
+        for (int i = 0; i < antall; i++) {
+            if (annenMengde.inneholder(tabell[i])) {
+                returnMengde.leggTil(tabell[i]);
+            }
+        }
+        return returnMengde;
+
+    }
+
+    @Override
+    public MengdeADT<T> union(MengdeADT<T> annenMengde){
+        TabellMengde<T> returnMengde = new TabellMengde<>();
+
+        returnMengde.leggTilAlleFra(this);
+        returnMengde.leggTilAlleFra(annenMengde);
+        return returnMengde;
+
+    }
+
+    @Override
+    public MengdeADT<T> minus(MengdeADT<T> annenMengde){
+        TabellMengde<T> returnMengde = new TabellMengde<>();
+
+        for (int i = 0; i < antall; i++) {
+            if (!annenMengde.inneholder(tabell[i])) {
+                returnMengde.leggTil(tabell[i]);
+            }
+        }
+        return returnMengde;
+
+    }
+
+    @Override
+    public void leggTil(T element) {
+        if (!(inneholder(element))) {
+            if (antall >= tabell.length) {
+                tabell = Arrays.copyOf(tabell, antall * 2);
+            }
+            tabell[antall] = element;
+            antall++;
+        }
+    }
+
+    @Override
+    public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+        T[] annenMengdeTabell = annenMengde.tilTabell();
+        for (T mengdeT : annenMengdeTabell) {
+            leggTil(mengdeT);
+        }
+    }
+
+    @Override
+    public T fjern(T element){
+        T temp = null;
+        for (int i = 0; i < antall; i++) {
+            if (tabell[i].equals(element)) {
+                temp = tabell[i];
+                tabell[i] = tabell[antall - 1];
+                tabell[antall - 1] = null;
+                antall--;
+                return temp;
+            }
+        }
+        return temp;
+    }
+
+    @Override
+    public T[] tilTabell() {
+		return Arrays.copyOf(tabell, antall);
+	}
+
+    @Override
+    public int antallElementer() {
+        return antall;
+    }
+
+}
---
+++ src/test/java/com/oppgaver/TabellMengdeTest.java
@@ -0,0 +1,116 @@
+package com.oppgaver;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TabellMengdeTest {
+    private TabellMengde<Integer> mengde1;
+    private TabellMengde<Integer> mengde2;
+
+    @BeforeEach
+    public void setup() {
+        mengde1 = new TabellMengde<>();
+        mengde2 = new TabellMengde<>();
+
+        mengde1.leggTil(1);
+        mengde1.leggTil(2);
+        mengde1.leggTil(3);
+
+        mengde2.leggTil(3);
+        mengde2.leggTil(4);
+        mengde2.leggTil(5);
+    }
+
+    @Test
+    public void testErTom() {
+        TabellMengde<Integer> tom = new TabellMengde<>();
+        assertTrue(tom.erTom());
+        tom.leggTil(10);
+        assertFalse(tom.erTom());
+    }
+
+    @Test
+    public void testLeggTilOgAntall() {
+        TabellMengde<Integer> m = new TabellMengde<>();
+        m.leggTil(1);
+        m.leggTil(2);
+        m.leggTil(2);
+
+        assertEquals(2, m.antallElementer());
+    }
+
+    @Test
+    public void testInneholder() {
+        assertTrue(mengde1.inneholder(1));
+        assertFalse(mengde1.inneholder(10));
+    }
+
+    @Test
+    public void testFjern() {
+        Integer fjernet = mengde1.fjern(2);
+        assertEquals(2, fjernet);
+        assertFalse(mengde1.inneholder(2));
+        assertEquals(2, mengde1.antallElementer());
+    }
+
+    @Test
+    public void testErDelmengdeAv() {
+        TabellMengde<Integer> del = new TabellMengde<>();
+        del.leggTil(1);
+        del.leggTil(2);
+
+        assertTrue(del.erDelmengdeAv(mengde1));
+        assertFalse(mengde1.erDelmengdeAv(del));
+    }
+
+    @Test
+    public void testErDisjunkt() {
+        TabellMengde<Integer> m = new TabellMengde<>();
+        m.leggTil(7);
+        m.leggTil(8);
+
+        assertTrue(m.erDisjunkt(mengde1));
+        assertFalse(mengde1.erDisjunkt(mengde2));
+    }
+
+    @Test
+    public void testSnitt() {
+        MengdeADT<Integer> snitt = mengde1.snitt(mengde2);
+
+        assertTrue(snitt.inneholder(3));
+        assertEquals(1, snitt.antallElementer());
+    }
+
+    @Test
+    public void testUnion() {
+        MengdeADT<Integer> union = mengde1.union(mengde2);
+
+        assertTrue(union.inneholder(1));
+        assertTrue(union.inneholder(2));
+        assertTrue(union.inneholder(3));
+        assertTrue(union.inneholder(4));
+        assertTrue(union.inneholder(5));
+
+        assertEquals(5, union.antallElementer());
+    }
+
+    @Test
+    public void testMinus() {
+        MengdeADT<Integer> minus = mengde1.minus(mengde2);
+
+        assertTrue(minus.inneholder(1));
+        assertTrue(minus.inneholder(2));
+        assertFalse(minus.inneholder(3));
+
+        assertEquals(2, minus.antallElementer());
+    }
+
+    @Test
+    public void testTilTabell() {
+        Integer[] tabell = mengde1.tilTabell();
+
+        assertEquals(3, tabell.length);
+        assertTrue(tabell[0] != null);
+    }
+}
